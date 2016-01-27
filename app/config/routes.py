df9a3f16ef036d8da88dc8aae18383583a055487
Sonya Panich <sonyapanich@gmail.com>
2016-01-27 10:57:30 -0800

    minor form edits

---
+++ app/config/routes.py
@@ -0,0 +1,57 @@
+"""
+    Routes Configuration File
+
+    Put Routing rules here
+"""
+from system.core.router import routes
+
+"""
+    This is where you define routes
+    
+    Start by defining the default controller
+    Pylot will look for the index method in the default controller to handle the base route
+
+    Pylot will also automatically generate routes that resemble: '/controller/method/parameters'
+    For example if you had a products controller with an add method that took one parameter 
+    named id the automatically generated url would be '/products/add/<id>'
+    The automatically generated routes respond to all of the http verbs (GET, POST, PUT, PATCH, DELETE)
+"""
+routes['default_controller'] = 'Blues'
+routes['/login'] = 'Blues#login'
+routes['GET']['/mentor_reg'] ='Blues#mentor_reg'
+routes['GET']['/mentored_reg'] = 'Blues#mentored_reg'
+routes['POST']['/tell_mentor'] = 'Blues#tellmr'
+routes['POST']['/tell_mentored'] = 'Blues#tellmd'
+routes['GET']['/dashboard'] = 'Blues#dash'
+routes['GET']['/thank_you'] ='Blues#thank'
+routes['POST']['/mentored_profile'] = 'Blues#profile'
+routes['POST']['/mentor_private'] = 'Blues#private'
+routes['GET']['/mentor_public'] = 'Blues#public'
+routes['GET']['/admin_view'] = 'Blues#admin'
+"""
+    You can add routes and specify their handlers as follows:
+
+    routes['VERB']['/URL/GOES/HERE'] = 'Controller#method'
+
+    Note the '#' symbol to specify the controller method to use.
+    Note the preceding slash in the url.
+    Note that the http verb must be specified in ALL CAPS.
+    
+    If the http verb is not provided pylot will assume that you want the 'GET' verb.
+
+    You can also use route parameters by using the angled brackets like so:
+    routes['PUT']['/users/<int:id>'] = 'users#update'
+
+    Note that the parameter can have a specified type (int, string, float, path). 
+    If the type is not specified it will default to string
+
+    Here is an example of the restful routes for users:
+
+    routes['GET']['/users'] = 'users#index'
+    routes['GET']['/users/new'] = 'users#new'
+    routes['POST']['/users'] = 'users#create'
+    routes['GET']['/users/<int:id>'] = 'users#show'
+    routes['GET']['/users/<int:id>/edit' = 'users#edit'
+    routes['PATCH']['/users/<int:id>'] = 'users#update'
+    routes['DELETE']['/users/<int:id>'] = 'users#destroy'
+"""
---
+++ app/models/Blue.py
@@ -0,0 +1,6 @@
+
+from system.core.model import Model
+
+class Blue(Model):
+    def __init__(self):
+        super(Blue, self).__init__()
