34fcd6c6531b67628d07a354b1a4749002399468
Andrew Wibel <andrew.wibel@gmail.com>
2016-01-27 16:40:26 -0800

    updated db and controller

--- Pylot/app/controllers/Blues.py
+++ Pylot/app/controllers/Blues.py
@@ -7,16 +7,54 @@ class Blues(Controller):
        self.load_model('Blue')
 
   def index(self):
-       return self.load_view('welcome.html')
+      return self.load_view('welcome.html')
        
   def login(self):
-       return self.load_view('login.html')
+      return self.load_view('login.html')
+
+  def signin(self):
+      signin_info = {
+      'username' : request.form['username']
+      'password' : request.form['password']
+      }
+      status = self.models['Blue'].signin(signin_info)
 
   def mentor_reg(self):
-       return self.load_view('mentor_reg.html')
+      return self.load_view('mentor_reg.html')
+
+  def mentor_create(self):
+      mentor_details = {
+        'first_name': request.form['first_name']
+        'last_name': request.form['last_name']
+        'email': request.form['email']
+        'zipcode': request.form['zip_code']
+        'username': request.form['username']
+        'password': request.form['passwod']
+        'confirm_password': request.form['confirm_password']
+        'form_q1': request.form['form_q1']
+        'form_q2': request.form['form_q1']
+        'bio': request.form['bio']
+      }
+      create_status = self.models['Blue'].create_mentor(mentor_details)
+      return redirect('/login')
 
   def mentored_reg(self):
-       return self.load_view('mentored_reg.html')
+      return self.load_view('mentored_reg.html')
+  
+  def mentored_create(self):
+      mentored_details = {
+        'first_name' : request.form['first_name']
+        'last_name' : request.form['last_name']
+        'email' : request.form['email']
+        'zipcode' : request.form['zip_code']
+        'username' : request.form['username']
+        'password' : request.form['passwod']
+        'confirm_password' : request.form['confirm_password']
+        'form_q1' : request.form['form_q1']
+        'bio' : request.form['bio']
+      }
+      create_status = self.models['Blue'].create_mentored(mentored_details)
+      return redirect('/login')
 
   def dash(self):
       return self.load_view('dashboard.html')
